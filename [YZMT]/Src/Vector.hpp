54a104933b220adc5073d35cb89f32697d743b37
Alan John Wickstead <onemanteamsoftware@gmail.com>
2020-04-10 18:09:43 +0100

    Day 007: Vector - Add vec2/vec3/vec4 Data Members

--- [YZMT]/Src/Vector.hpp
+++ [YZMT]/Src/Vector.hpp
@@ -4,11 +4,25 @@
 
 namespace YZMT {
     struct vec2 {
+        union {
+            struct { float x; float y; };
+            float v[2];
+        };
     };
     
     struct vec3 {
+        union {
+            struct { float x; float y; float z; };
+            struct { vec2 xy; float z; };
+            float v[3];
+        };
     };
     
     struct vec4 {
+        union {
+            struct { float x; float y; float z; float w; };
+            struct { vec3 xyz; float w; };
+            float v[4];
+        };
     };
 }
