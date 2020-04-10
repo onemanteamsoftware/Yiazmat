31dacb94844ba63a045b39582d87ed9663f2e285
Alan John Wickstead <onemanteamsoftware@gmail.com>
2020-04-10 18:28:40 +0100

    Day 007: Matrix - Add mat3/mat4 Data Members

--- [YZMT]/Src/Matrix.hpp
+++ [YZMT]/Src/Matrix.hpp
@@ -6,8 +6,16 @@
 
 namespace YZMT {
     struct mat3 {
+        union {
+            float Elements[3 * 3];
+            vec3 Columns[3];
+        };
     };
     
     struct mat4 {
+        union {
+            float Elements[4 * 4];
+            vec4 Columns[4];
+        };
     };
 }
