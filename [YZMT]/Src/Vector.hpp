dcfdfbb4a278410a657116392ef10eaa714c9cb4
Alan John Wickstead <onemanteamsoftware@gmail.com>
2020-04-11 13:36:39 +0100

    Day 008: Vector - Add Default Constructors to vec2/vec3/vec4 Structs

--- [YZMT]/Src/Vector.hpp
+++ [YZMT]/Src/Vector.hpp
@@ -8,6 +8,8 @@ namespace YZMT {
             struct { float x; float y; };
             float v[2];
         };
+        
+        vec2() : x { 0.0f }, y { 0.0f } {}
     };
     
     struct vec3 {
@@ -16,6 +18,8 @@ namespace YZMT {
             struct { vec2 xy; float z; };
             float v[3];
         };
+        
+        vec3() : x { 0.0f }, y { 0.0f }, z { 0.0f } {}
     };
     
     struct vec4 {
@@ -24,5 +28,7 @@ namespace YZMT {
             struct { vec3 xyz; float w; };
             float v[4];
         };
+        
+        vec4() : x { 0.0f }, y { 0.0f }, z { 0.0f }, w { 0.0f } {}
     };
 }
