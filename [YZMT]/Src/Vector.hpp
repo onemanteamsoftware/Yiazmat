2dcbc68b4a2e731b0b3e61cc3ab37a0b83150bd8
Alan John Wickstead <onemanteamsoftware@gmail.com>
2020-04-15 15:02:46 +0100

    Day 012: Vector - Add Magnitude() to vec2/vec3/vec4 Structs

--- [YZMT]/Src/Vector.hpp
+++ [YZMT]/Src/Vector.hpp
@@ -2,6 +2,8 @@
 /*github.com/onemanteamsoftware*/
 #pragma once
 
+#include <cmath>
+
 namespace YZMT {
     struct vec2 {
         union {
@@ -11,6 +13,8 @@ namespace YZMT {
         
         vec2() : x { 0.0f }, y { 0.0f } {}
         explicit vec2(float x, float y = 0.0f) : x { x }, y { y } {}
+        
+        float Magnitude() const { return std::sqrt(x * x + y * y); }
         
         vec2& operator*=(float s) { x *= s; y *= s; return *this; }
         vec2& operator/=(float s) { x /= s; y /= s; return *this; }
@@ -27,6 +31,8 @@ namespace YZMT {
         vec3(const vec2& v, float z = 0.0f) : x { v.x }, y { v.y }, z { z } {}
         vec3(float x, float y, float z = 0.0f) : x { x }, y { y }, z { z } {}
         
+        float Magnitude() const { return std::sqrt(x * x + y * y + z * z); }
+        
         vec3& operator*=(float s) { x *= s; y *= s; z *= s; return *this; }
         vec3& operator/=(float s) { x /= s; y /= s; z /= s; return *this; }
     };
@@ -41,6 +47,8 @@ namespace YZMT {
         vec4() : x { 0.0f }, y { 0.0f }, z { 0.0f }, w { 0.0f } {}
         vec4(const vec3& v, float w = 0.0f) : x { v.x }, y { v.y }, z { v.z }, w { w } {}
         vec4(float x, float y, float z, float w = 0.0f) : x { x }, y { y }, z { z }, w { w } {}
+        
+        float Magnitude() const { return std::sqrt(x * x + y * y + z * z + w * w); }
         
         vec4& operator*=(float s) { x *= s; y *= s; z *= s; w *= s; return *this; }
         vec4& operator/=(float s) { x /= s; y /= s; z /= s; w /= s; return *this; }
