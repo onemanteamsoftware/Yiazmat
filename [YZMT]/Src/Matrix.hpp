34229e13526bf83975c991d0b1b6e381a49bd38c
Alan John Wickstead <onemanteamsoftware@gmail.com>
2020-04-27 06:38:31 +0100

    Day 024: Matrix - Add Default Constructors to mat3/mat4 Structs

--- [YZMT]/Src/Matrix.hpp
+++ [YZMT]/Src/Matrix.hpp
@@ -10,6 +10,8 @@ namespace YZMT {
             float Elements[3 * 3];
             vec3 Columns[3];
         };
+        
+        mat3() : Elements { 0.0f } {}
     };
     
     struct mat4 {
@@ -17,5 +19,7 @@ namespace YZMT {
             float Elements[4 * 4];
             vec4 Columns[4];
         };
+        
+        mat4() : Elements { 0.0f } {}
     };
 }
